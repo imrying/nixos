5a2d4ba8a9df9ae5e5f9e7fe9b5da0f9955aa218
imrying <philiprying@gmail.com>
2024-01-29 17:13:37 +0100

    remove commented stuff

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -123,26 +123,13 @@
     valgrind
     libsForQt5.kcachegrind
     tor-browser
-    slack
     nodejs
+    telegram-desktop
     wl-clipboard
   ];
   services.fwupd.enable = true;
   # services.fprintd.enable = true;
-  
-  # programs.hyprland = {
-  # 	enable = true;
-  #   xwayland.enable = true;
-  # };
-
-  # Enable mullvad vpn
-  # services.mullvad-vpn.enable = true;
-
-  
-  # services.emacs = {
-  #   enable = true;
-  #   package = pkgs.emacs; # replace with emacs-gtk, or a version provided by the community overlay if desired.
-  # };
+  # virtualisation.docker.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
